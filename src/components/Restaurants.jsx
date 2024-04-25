373f020a016278d09bf11fbab3a0706f8d24a14a
test <pranayshah2206@gmail.com>
2024-04-25 14:39:55 +0530

    created different filters

--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -10,6 +10,11 @@ const Restaurants = () => {
   const [restaurantsList, setRestaurantsList] = useState([]);
   const currentLocation = useSelector((store) => store.location.loc);
 
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  //state for making active button
+  const [activeButton, setActiveButton] = useState("button1");
+
   const VegRestaurantCard = withVegLabel(RestaurantCard);
 
   useEffect(() => {
@@ -26,11 +31,55 @@ const Restaurants = () => {
       );
       const json = await data.json();
       setRestaurantsList(
-        json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+        json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+      );
+      setFilteredRestaurants(
+        json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
       );
     } catch (error) {
       console.error("Error fetching data:", error);
     }
+  };
+
+  const handleClearFilter = () => {
+    setFilteredRestaurants(restaurantsList);
+  };
+
+  const handleFastDelivery = () => {
+    const fastDelivery = restaurantsList.filter(
+      (rest) => rest.info.sla.deliveryTime < 30
+    );
+    setFilteredRestaurants(fastDelivery);
+  };
+
+  const handleTopRating = () => {
+    const topRated = restaurantsList.filter((rest) => rest.info.avgRating > 4);
+    setFilteredRestaurants(topRated);
+  };
+
+  const handleOnlyVeg = () => {
+    const onlyVeg = restaurantsList.filter((rest) => rest.info.veg == true);
+    setFilteredRestaurants(onlyVeg);
+  };
+
+  const handleLessThan300 = () => {
+    const lessThan300 = restaurantsList.filter(
+      (rest) => rest.info.costForTwo.match(/\d+/)[0] < 300 == true
+    );
+    setFilteredRestaurants(lessThan300);
+  };
+
+  const handleBetween300and600 = () => {
+    const between300and600 = restaurantsList.filter(
+      (rest) =>
+        rest.info.costForTwo.match(/\d+/)[0] >= 300 &&
+        rest.info.costForTwo.match(/\d+/)[0] < 600
+    );
+    setFilteredRestaurants(between300and600);
+  };
+
+  const handleClick = (buttonName) => {
+    setActiveButton(buttonName === activeButton ? null : buttonName);
   };
 
   return restaurantsList.length == 0 ? (
@@ -43,26 +92,88 @@ const Restaurants = () => {
           Restaurants with online food delivery in {currentLocation.city}
         </div>
         <div className="flex gap-8 my-5">
-          <button className="border p-2 shadow-sm rounded-2xl">
+          <button
+            className={`border py-2 px-3 shadow-sm rounded-[18px] ${
+              activeButton === "button1"
+                ? "border-4 font-bold text-[#fc8019]"
+                : ""
+            }`}
+            onClick={() => {
+              handleClearFilter();
+              handleClick("button1");
+            }}
+          >
+            Clear Filter
+          </button>
+          <button
+            className={`border p-2 shadow-sm rounded-2xl ${
+              activeButton === "button2"
+                ? "border-4 font-bold text-[#fc8019]"
+                : ""
+            }`}
+            onClick={() => {
+              handleFastDelivery();
+              handleClick("button2");
+            }}
+          >
             Fast Delivery
           </button>
-          <button className="border p-2 shadow-sm rounded-2xl">
-            New on Swiggy
-          </button>
-          <button className="border p-2 shadow-sm rounded-2xl">
+          <button
+            className={`border p-2 shadow-sm rounded-2xl ${
+              activeButton === "button3"
+                ? "border-4 font-bold text-[#fc8019]"
+                : ""
+            }`}
+            onClick={() => {
+              handleTopRating();
+              handleClick("button3");
+            }}
+          >
             Ratings 4.0+
           </button>
-          <button className="border p-2 shadow-sm rounded-2xl">Pure Veg</button>
-          <button className="border p-2 shadow-sm rounded-2xl">
+          <button
+            className={`border p-2 shadow-sm rounded-2xl ${
+              activeButton === "button4"
+                ? "border-4 font-bold text-[#fc8019]"
+                : ""
+            }`}
+            onClick={() => {
+              handleOnlyVeg();
+              handleClick("button4");
+            }}
+          >
+            Pure Veg
+          </button>
+          <button
+            className={`border p-2 shadow-sm rounded-2xl ${
+              activeButton === "button5"
+                ? "border-4 font-bold text-[#fc8019]"
+                : ""
+            }`}
+            onClick={() => {
+              handleLessThan300();
+              handleClick("button5");
+            }}
+          >
             Less than Rs. 300
           </button>
-          <button className="border p-2 shadow-sm rounded-2xl">
+          <button
+            className={`border p-2 shadow-sm rounded-2xl ${
+              activeButton === "button6"
+                ? "border-4 font-bold text-[#fc8019]"
+                : ""
+            }`}
+            onClick={() => {
+              handleBetween300and600();
+              handleClick("button6");
+            }}
+          >
             Rs. 300 - Rs. 600
           </button>
         </div>
 
         <div className="grid grid-cols-4 gap-3">
-          {restaurantsList.map((restaurant) => (
+          {filteredRestaurants.map((restaurant) => (
             <React.Fragment key={restaurant.info.id}>
               {restaurant.info.veg ? (
                 <VegRestaurantCard resList={restaurant} />
