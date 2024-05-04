d532215b28f43ecd19f37558041a5f5aa642808b
test <pranayshah2206@gmail.com>
2024-05-04 11:15:17 +0530

    updated category url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -48,7 +48,7 @@ export const locationObject = [
 ];
 
 export const CATEGORY_IMG_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
 
 export const RESTAURANT_LIST =
   "https://swiggato-server.onrender.com/api/restaurants?lat=19.1889541&lng=72.835543&page_type=DESKTOP_WEB_LISTING";
